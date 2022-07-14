d6d0773120dc952ce53763750dd558153d28ce27
OLAKUNLE IDOWU <kunleidowu04@yahoo.com>
2022-07-14 00:37:27 +0000

    challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,16 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+function  sumOfNumbers(arrayOfNumbers) {
+   let sum = 0;
+   for (let i = 0; i < arrayOfNumbers.length; i++) 
+   {
+     sum = sum + arrayOfNumbers[i];
+    }
+  return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+ assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -27,10 +33,19 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
+ 
+ 
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+   let EvenNumbers = 0
+   for (let i = 0; i < arrayOfNumbers.length; i++) 
+   {if (arrayOfNumbers[i] % 2 ===0) {
+       EvenNumbers++; }
+    }
+  { return EvenNumbers;
+  }
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
